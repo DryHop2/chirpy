531291af9fbceebc9e8e817a7eea43ddfa9888f8
DryHop2 <tyler.savon.smith@gmail.com>
2025-04-29 04:37:19 -0600

    Add DB connection setup and UUID dependency for SQLC integration

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+	"os"
 	"sync/atomic"
+
+	"github.com/DryHop2/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	DB             *database.Queries
 }
 
 func main() {
-	apiCfg := &apiConfig{}
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Could not connect to DB: %v", err)
+	}
+
+	dbQueries := database.New(db)
+
+	apiCfg := &apiConfig{
+		fileServerHits: atomic.Int32{},
+		DB:             dbQueries,
+	}
 
 	router := setupRouter(apiCfg)
 	startServer(router)
